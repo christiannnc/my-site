52d43a658f3da59060ede1f6a878c62d8c12abb9
christiannnc <c917unc@gmail.com>
2022-11-26 14:35:04 -0500

    added console messages and made responsive changes for mobile

--- src/screens/Hello.js
+++ src/screens/Hello.js
@@ -7,8 +7,21 @@ import "../css/hello.css";
 
 export default function Hello() {
     useEffect(() => {
-        console.log("%chi there!", "color: #5AB1BB");
+        console.log(
+            "%chi there!",
+            "color: #5AB1BB; font-size: 20px; font-weight: 600"
+        );
         console.log("if you're seeing this, you're probably a developer");
+        console.log("");
+        console.log(
+            "%ci hope to be a developer one day too :)",
+            "font-weight: 600"
+        );
+        console.log("");
+        console.log(
+            "if you'd like, feel free to email me at christian@christianmcox.com " +
+                "to let me know what you think about this site, or what i could improve on!"
+        );
     }, []);
 
     return (
